86c0a41f980c35f0b89143277295424bda5cc3f4
Ramazan Güçlü <ramazanguclu33@gmail.com>
2018-02-13 23:47:21 +0300

    initial data

---
+++ WebApplication1/WebApplication1/DAL/PenaltyCalculationInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class PenaltyCalculationInitializer : CreateDatabaseIfNotExists<PenaltyCalculationContext>
+    {
+        protected override void Seed(PenaltyCalculationContext context)
+        {
+            List<Countries> countries = new List<Countries>
+            {
+                new Countries {Name = "Turkey"},
+                new Countries {Name = "Dubai"}
+            };
+
+            countries.ForEach(c => context.Countries.Add(c));
+            context.SaveChanges();
+        }
+    }
+}
